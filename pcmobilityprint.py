6d6db8c93180c3e932f3709325466786e4bceccf
Maksymilian B. Knust <mbknust@users.noreply.github.com>
2026-03-06 11:05:30 +0100

    fix: handle spaces in printer name

--- pcmobilityprint.py
+++ pcmobilityprint.py
@@ -3,6 +3,7 @@ import json
 import ssl
 import socket
 import base64
+import urllib.parse
 import urllib.request
 import subprocess
 
@@ -109,8 +110,8 @@ class PCMobilityPrint:
 		b64creds = base64.b64encode(f"{self.username}:{self.password}".encode()).strip().decode()
 
 		try:
-
-			req = urllib.request.Request(f"{self.baseurl}/printer-url?printerName={name}&serverName={self.socket}")
+			params = urllib.parse.urlencode({"printerName": name, "serverName": self.socket})
+			req = urllib.request.Request(f"{self.baseurl}/printer-url?{params}")
 			req.add_header("Authorization", f"Basic {b64creds}")
 			p = urllib.request.urlopen(req, context=ctx)
 
@@ -149,7 +150,7 @@ class PCMobilityPrint:
 		try:
 
 			# TODO: -L<location> and -D<description> may not always be the same thing
-			cmd = ["/usr/sbin/lpadmin", "-p", name, "-E", "-L", desc, "-D", desc, "-v", url]
+			cmd = ["/usr/sbin/lpadmin", "-p", name.replace(' ', '_'), "-E", "-L", desc, "-D", desc, "-v", url]
 			subprocess.call(cmd)
 
 			return True
